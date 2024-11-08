456e99ce8278d70542aa7920d09858c5906ce6b5
Rafael Arrifano <rafael.arrifano@gmail.com>
2024-11-07 21:21:11 -0300

    tmux stuff

--- .config/nvim/lua/arri/keybinds.lua
+++ .config/nvim/lua/arri/keybinds.lua
@@ -23,8 +23,3 @@ vim.keymap.set('n', '<M-h>', '<CMD>NavigatorLeft<CR>', { silent = true })
 vim.keymap.set('n', '<M-j>', '<CMD>NavigatorDown<CR>', { silent = true })
 vim.keymap.set('n', '<M-k>', '<CMD>NavigatorUp<CR>', { silent = true })
 vim.keymap.set('n', '<M-l>', '<CMD>NavigatorRight<CR>', { silent = true })
-
--- vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', { silent = true })
--- vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', { silent = true })
--- vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', { silent = true })
--- vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', { silent = true })
