9ca69d0738dc9136afa85597a8d713c2eb80716a
Rafael Arrifano <rafael.arrifano@gmail.com>
2024-11-15 11:57:10 -0300

    shame on you git plugins

--- .config/nvim/lua/arri/plugins.lua
+++ .config/nvim/lua/arri/plugins.lua
@@ -2,18 +2,13 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
-Plug ('airblade/vim-gitgutter')
+Plug ('morhetz/gruvbox')
 Plug ('christoomey/vim-tmux-navigator')
 Plug ('nvim-lua/plenary.nvim')
 Plug ('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
 Plug ('nvim-treesitter/nvim-treesitter', { ['tag'] = 'v0.7.2' })
-Plug ('morhetz/gruvbox')
-Plug ('tpope/vim-fugitive')
-Plug ('tpope/vim-sensible')
-Plug ('tpope/vim-surround')
 
 vim.call('plug#end')
 
 vim.cmd('colorscheme gruvbox')
-
 require'nvim-treesitter.configs'.setup { highlight = { enable = true } }
