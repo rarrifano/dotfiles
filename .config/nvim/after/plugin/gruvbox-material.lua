44705c1da06477c6bcd269ddaabe0bab3f6f07ed
Rafael Arrifano <rafael.arrifano@gmail.com>
2025-03-09 13:57:41 -0300

    Gruv my box baby

---
+++ .config/nvim/after/plugin/gruvbox-material.lua
@@ -0,0 +1,6 @@
+vim.g.gruvbox_material_foreground='original'
+vim.g.gruvbox_material_background='hard'
+vim.g.gruvbox_material_better_performance=1
+vim.g.gruvbox_material_transparent_background=2
+
+vim.cmd('colorscheme gruvbox-material')
