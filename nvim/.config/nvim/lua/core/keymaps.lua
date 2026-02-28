d6667728b2d6a5ccc50a0178110ffd920785ccd1
Rafael Arrifano <rafael.arrifano@gmail.com>
2026-02-28 11:53:06 -0300

    Separate LSP keymaps (<leader>l) from git keymaps (<leader>g)

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -23,11 +23,11 @@ map('n', '<leader><leader>', '<cmd>Telescope buffers<CR>', { desc = 'Buffers' })
 map('n', '<leader>:', '<cmd>Telescope commands<CR>', { desc = 'Commands' })
 
 -- LSP
-map('n', '<leader>ld', '<cmd>Telescope diagnostics<CR>', { desc = 'Diagnostics list' })
-map('n', '<leader>gr', '<cmd>Telescope lsp_references<CR>', { desc = 'References' })
-map('n', '<leader>gi', '<cmd>Telescope lsp_implementations<CR>', { desc = 'Implementations' })
-map('n', '<leader>gd', '<cmd>Telescope lsp_definitions<CR>', { desc = 'Definitions' })
-map('n', '<leader>gt', '<cmd>Telescope lsp_type_definitions<CR>', { desc = 'Type definitions' })
+map('n', '<leader>lx', '<cmd>Telescope diagnostics<CR>', { desc = 'Diagnostics list' })
+map('n', '<leader>lr', '<cmd>Telescope lsp_references<CR>', { desc = 'References' })
+map('n', '<leader>li', '<cmd>Telescope lsp_implementations<CR>', { desc = 'Implementations' })
+map('n', '<leader>ld', '<cmd>Telescope lsp_definitions<CR>', { desc = 'Definitions' })
+map('n', '<leader>lt', '<cmd>Telescope lsp_type_definitions<CR>', { desc = 'Type definitions' })
 
 -- Diagnostics
 map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Prev diagnostic' })
