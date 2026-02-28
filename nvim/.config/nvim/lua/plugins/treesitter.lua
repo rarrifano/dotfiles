2c4d5814ce0c604aaeaad221c5847c90aeb74791
Rafael Arrifano <rafael.arrifano@gmail.com>
2026-02-28 11:54:25 -0300

    Fix treesitter config for new nvim-treesitter main branch API

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,10 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = { 'terraform', 'python', 'bash', 'dockerfile', 'yaml', 'json', 'markdown', 'lua' },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+    main = 'nvim-treesitter',
+    opts = {
+        ensure_installed = { 'terraform', 'python', 'bash', 'dockerfile', 'yaml', 'json', 'markdown', 'lua' },
+        highlight = { enable = true },
+        indent = { enable = true },
+    },
 }
